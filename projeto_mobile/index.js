abf42d321d814669018a3e33e2e6eb748c8c37d0
Leonardo stroher <130716072+Leonardo-stroher@users.noreply.github.com>
2023-10-11 22:33:47 -0300

    11/10

--- projeto_mobile/index.js
+++ projeto_mobile/index.js
@@ -3,9 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App2 from './App2';
+import App from './App';
 import {name as appName} from './app.json';
 
 
-AppRegistry.registerComponent(appName, () => App2);
+AppRegistry.registerComponent(appName, () => App);
 
